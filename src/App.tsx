f067a7756820c206ee1e14e8712c37911f98ec5f
missstaff <shawnastaff@gmail.com>
2024-04-11 08:18:38 -0700

    01-main-navigation -- Added: Navigation menu for the main site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainNavigation from "./components/UI/Navigation/MainNavigationMenu/MainNavigationMenu";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Projects from "./pages/projects/Projects";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC  = () => {
+ return (
+   <Router>
+     <MainNavigation />
+     <main className="main-content">
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/contact" element={<Contact />} />
+         <Route path="/projects" element={<Projects />} />
+         {/* Define other routes that you need*/}
+       </Routes>
+     </main>
+   </Router>
+ );
+};
 
 export default App;
---
+++ src/components/UI/Navigation/MainNavigationMenu/MainNavigationMenu.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+import "./MainNavigationMenu.css";
+
+const MainNavigationMenu: React.FC = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenuOnMobile = () => {
+    if (window.innerWidth <= 1150) {
+      setShowMenu(false);
+    }
+  };
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <NavLink to="/" className="nav__logo">
+          Shawna Staff
+        </NavLink>
+
+        <div
+          className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+          id="nav-menu"
+        >
+          <ul className="nav__list">
+            <li className="nav__item">
+              <NavLink to="/" className="nav__link" onClick={closeMenuOnMobile}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/about"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                About
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/projects"
+                className="nav__link"
+                onClick={closeMenuOnMobile}
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li className="nav__item">
+              <NavLink
+                to="/contact"
+                className="nav__link nav__cta"
+                onClick={closeMenuOnMobile}
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav__close" id="nav-close" onClick={toggleMenu}>
+            <IoClose />
+          </div>
+        </div>
+
+        <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+          <IoMenu />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigationMenu;
